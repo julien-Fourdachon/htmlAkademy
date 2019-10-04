0880c011e68c97eb3ec4faebd08433ce8d5dfbca
julien-Fourdachon <julien.fourdachon@gmail.com>
2019-10-04 14:46:09 +0200

    validation w3c

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 
 $('.filter-js').click(function () {
     var toggleColor = $(this).attr("data-image");
-    var modal = $(this).attr("datas");
+    var modal = $(this).attr("data-modal");
 
     $(".picture").removeClass("selected-color");
     $("[data-image=" + toggleColor + "]").addClass('selected-color');
 
     $(".content, .span").removeClass("show");
-    $("[datas=" + modal + "]").addClass('show');
+    $("[data-modal=" + modal + "]").addClass('show');
 });
 
 
